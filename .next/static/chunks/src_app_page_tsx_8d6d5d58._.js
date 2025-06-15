2a84e930b1fd2499d106b9c8a1e5dbe96c6ee649
geha9999 <geha9999@yahoo.com>
2025-06-15 22:02:40 +0700

    Add .next/static/chunks/src_app_page_tsx_8d6d5d58._.js 1749999758442

---
+++ .next/static/chunks/src_app_page_tsx_8d6d5d58._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_dd86a640._.js"
+],
+    source: "dynamic"
+});
