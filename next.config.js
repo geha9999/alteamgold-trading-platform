55a39a24c0fd3a2869e2326bbec9f3eeca3ef57d
geha9999 <geha9999@yahoo.com>
2025-06-15 22:02:21 +0700

    Add next.config.js 1749999739782

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {
+    experimental: true,
+  },
+  allowedDevOrigins: ['https://qzx6qw-8000.csb.app'],
+};
